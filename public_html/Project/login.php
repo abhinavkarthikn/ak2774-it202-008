ec5d00adb66bae40cc201272ce7492d0117e862a
abhik <abhikart02@gmail.com>
2024-02-28 21:38:40 -0500

    housekeeping

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -1,10 +1,10 @@
 <?php
-require_once(__DIR__ . "/../../partials/nav.php");
+require(__DIR__ . "/../../partials/nav.php");
 ?>
 <form onsubmit="return validate(this)" method="POST">
     <div>
         <label for="email">Email</label>
-        <input id="email" type="email" name="email" required />
+        <input type="email" name="email" required />
     </div>
     <div>
         <label for="pw">Password</label>
@@ -16,46 +16,48 @@ require_once(__DIR__ . "/../../partials/nav.php");
     function validate(form) {
         //TODO 1: implement JavaScript validation
         //ensure it returns false for an error and true for success
+
         return true;
     }
 </script>
 <?php
- //TODO 2: add PHP Code
- if(isset($_POST["email"]) && isset($_POST["password"]) ){
-    $email = se($_POST,"email","", false);//$_POST["email"];
-    $password = se($_POST, "password","", false);//$_POST["password"];
-    
-    
-    //TODO3: validate/use
-    $hasError=false;
-    if(empty($email)){
-        flash("Email must not be empty <br>");
-        $hasError=true;
+//TODO 2: add PHP Code
+if (isset($_POST["email"]) && isset($_POST["password"])) {
+    $email = se($_POST, "email", "", false);
+    $password = se($_POST, "password", "", false);
+
+    //TODO 3
+    $hasError = false;
+    if (empty($email)) {
+        flash("Email must not be empty");
+        $hasError = true;
     }
     //sanitize
     //$email = filter_var($email, FILTER_SANITIZE_EMAIL);
-    $email= sanitize_email($email);
+    $email = sanitize_email($email);
     //validate
-    /*if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-        flash("Please enter a valid email <br>");
-        $hasError=true;
+    /*if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        flash("Invalid email address");
+        $hasError = true;
     }*/
-    if(!is_valid_email($email)){
-        flash("Please enter a valid email <br>");
-        $hasError=true;
+    if (!is_valid_email($email)) {
+        flash("Invalid email address");
+        $hasError = true;
     }
-    if(empty($password)){
-        flash("Password must not be empty <br>");
-        $hasError=true;
+    if (empty($password)) {
+        flash("password must not be empty");
+        $hasError = true;
     }
-    if(strlen($password)<8){
-        flash("Password must be at least 8 characters long <br>");
-        $hasError=true;
+    if (!is_valid_password($password)) {
+        flash("Password too short");
+        $hasError = true;
     }
-    if(!$hasError){
+    if (!$hasError) {
+        //flash("Welcome, $email");
         //TODO 4
         $db = getDB();
-        $stmt = $db->prepare("SELECT id, email, username, password from Users where email = :email");
+        $stmt = $db->prepare("SELECT id, email, username, password from Users 
+        where email = :email");
         try {
             $r = $stmt->execute([":email" => $email]);
             if ($r) {
@@ -64,8 +66,9 @@ require_once(__DIR__ . "/../../partials/nav.php");
                     $hash = $user["password"];
                     unset($user["password"]);
                     if (password_verify($password, $hash)) {
-                        flash("Welcome $email");
-                        $_SESSION["user"]= $user;
+                        //flash("Weclome $email");
+                        $_SESSION["user"] = $user; //sets our session data from db
+                        flash("Welcome, " . get_username());
                         die(header("Location: home.php"));
                     } else {
                         flash("Invalid password");
@@ -77,9 +80,8 @@ require_once(__DIR__ . "/../../partials/nav.php");
         } catch (Exception $e) {
             flash("<pre>" . var_export($e, true) . "</pre>");
         }
-        
     }
-
- }
+}
 ?>
-<?php require_once(__DIR__ . "/../../partials/flash.php"); 
+<?php 
+require(__DIR__."/../../partials/flash.php");
