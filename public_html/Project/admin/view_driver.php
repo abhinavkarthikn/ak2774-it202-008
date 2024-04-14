5bfd29fe12c06d78b28d5e989cb51a5da233195e
abhik <abhikart02@gmail.com>
2024-04-14 11:31:36 -0400

    Single view page completed

---
+++ public_html/Project/admin/view_driver.php
@@ -0,0 +1,51 @@
+<?php
+
+require(__DIR__ . "/../../../partials/nav.php");
+
+if (!has_role("Admin")) {
+    flash("You do not have permission to view this page", "warning");
+    die(header("location: $BASE_PATH" . "/home.php"));
+}
+
+$id = se($_GET, "id", -1, false);
+
+$driverData=[];
+if($id>-1){
+    $db=getDB();
+    $query="SELECT name, abbr, image, nationality, country, birthdate, birthplace, number, grands_prix_entered, world_championships, podiums, highest_race_finish, highest_grid_position, career_points FROM `Drivers` WHERE id=:id";
+    try{
+        $stmt=$db->prepare($query);
+        $stmt->execute([":id"=>$id]);
+        $driverData=$stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    catch(PDOException $e){
+        error_log("Error fetching driver data: " . var_export($e, true));
+        flash("Error fetching driver data", "danger");
+    }
+}
+else{
+    flash("Invalid id passed", "danger");
+    die(header("Location: " . get_url("admin/list_drivers.php")));
+}
+?>
+
+<div class="container mt-4 d-flex justify-content-center">
+    <div class="card" style="width: 18rem;">
+        <?php if(!empty($driverData["image"])):?>
+            <img src="<?php echo $driverData["image"];?>" class="card-img-top" alt="Driver Image">
+        <?php endif;?>
+
+        <div class="card-body">
+            <h5 class="card-title"><?php echo($driverData["name"]);?></h5>
+            <p class="card-text">Driver Number: <?php safer_echo($driverData["number"]);?></p>
+            <p class="card-text">Birthdate: <?php safer_echo($driverData["birthdate"]);?></p>
+            <p class="card-text">Country: <?php safer_echo($driverData["country"]);?></p>
+            <p class="card-text">GP's Entered: <?php safer_echo($driverData["grands_prix_entered"]);?></p>
+            <p class="card-text">World Championships: <?php safer_echo($driverData["world_championships"]);?></p>
+            <p class="card-text">Podiums: <?php safer_echo($driverData["podiums"]);?></p>
+            <p class="card-text">Career Points: <?php safer_echo($driverData["career_points"]);?></p>
+        </div>
+    </div>
+</div>
+
+<?php require(__DIR__ . "/../../../partials/flash.php");?>
