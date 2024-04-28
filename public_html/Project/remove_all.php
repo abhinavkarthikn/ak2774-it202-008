71a95780889fdad6c7a46b2f5c8a7b99336d1f5f
abhik <abhikart02@gmail.com>
2024-04-28 16:55:43 -0400

    Minor tweaks completed

--- public_html/Project/remove_all.php
+++ public_html/Project/remove_all.php
@@ -2,18 +2,21 @@
 require(__DIR__ . "/../../partials/nav.php");
 
 $db = getDB();
-$username = $_GET["username"] ?? null; // Fetch username from query string
+
+$username = $_GET["username"];
 if ($username) {
-    $query = "DELETE FROM UserDrivers WHERE user_id IN (SELECT id FROM Users WHERE username = :username)";
-    $params = [":username" => $username];
+    $query = "DELETE FROM UserDrivers WHERE user_id IN (SELECT id FROM Users WHERE username LIKE :username)";
+    $params = [":username" => "%$username%"];
+    
     try {
         $stmt = $db->prepare($query);
         $stmt->execute($params);
-        flash("All drivers favorited by user removed successfully", "success");
+        flash("All drivers favorited by user(s)  removed successfully", "success");
     } catch (PDOException $e) {
         error_log("Error removing drivers: " . $e->getMessage());
         flash("Error removing drivers", "danger");
     }
 }
+
 redirect("my_drivers.php");
 ?>
